803eb69f4c525378ba647de8aebbd1e0c743b650
Logan Lieberg <loganlieberg@Logans-MacBook-Pro.local>
2016-05-05 10:46:54 -0500

    update

--- script.js
+++ script.js
@@ -9,16 +9,20 @@ var orange = new FruitCreation ('orange', 1);
 var bear = new FruitCreation ('bear', 1);
 
 var fruitPriceArray = [apple.marketPrice, banana.marketPrice, bear.marketPrice, orange.marketPrice];
-var fruitNameArray = [apple, banana, bear, orange];
+var fruitNameArray = [apple, banana, orange, bear];
 console.log(fruitPriceArray);
 
-$('#fruitBasket').append('<span>' + apple.name + ' ' + apple.marketPrice + ' ' + '</span>');
-$('#fruitBasket').append('<span>' + banana.name + ' ' + banana.marketPrice + ' ' + '</span>');
-$('#fruitBasket').append('<span>' + orange.name + ' ' + orange.marketPrice + ' ' + '</span>');
-$('#fruitBasket').append('<span>' + bear.name + ' ' + bear.marketPrice + ' ' + '</span>');
+$('#fruitBasket').append('<span>' + apple.name + '</span>');
+$('#fruitBasket').append('<span id="apple">' + apple.marketPrice + '</span>');
+$('#fruitBasket').append('<span>' + banana.name + '</span>');
+$('#fruitBasket').append('<span id="banana">' + banana.marketPrice + '</span>');
+$('#fruitBasket').append('<span>' + orange.name + '</span>');
+$('#fruitBasket').append('<span id="orange">' + orange.marketPrice + '</span>');
+$('#fruitBasket').append('<span>' + bear.name + '</span>');
+$('#fruitBasket').append('<span id="bear">' + bear.marketPrice + '</span>');
 var priceChange = window.setInterval(function (){randomPrice(fruitPriceArray);}, 15000);
+var domChange = window.setInterval(function (){intervalUpdate()}, 15000);
 //$('#fruitBasket span').replaceWith('<span>' + apple.name + priceChange + '</span>');
-
 console.log(priceChange);
 function randomPrice (priceInterval) {
 	for(var i = 0; i < priceInterval.length; i++){
@@ -42,5 +46,12 @@ function randomNumber () {
 
 	//randomize a number between 0 and 0.50
 
+}
+
+function intervalUpdate () {
+	$('#apple').text(apple.marketPrice.toFixed(2));
+	$('#banana').text(banana.marketPrice.toFixed(2));
+	$('#orange').text(orange.marketPrice.toFixed(2));
+	$('#bear').text(bear.marketPrice.toFixed(2));
 }
 });
